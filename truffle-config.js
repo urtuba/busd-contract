83369945cfa94f2f4ab05dbf70b1648b2f733a73
Samed Kahyaoglu <32146055+urtuba@users.noreply.github.com>
2021-01-06 21:24:25 +0300

    some working environment fixes

--- truffle-config.js
+++ truffle-config.js
@@ -1,7 +1,15 @@
-var HDWalletProvider = require("truffle-hdwallet-provider");
+var HDWalletProvider = require("@truffle/hdwallet-provider");
 const mnemonic = "";
 const walletChildNum = 0;
 const networkAddress = "https://mainnet.infura.io/v3/<your-api-key>";
+
+const PrivateKeyProvider = require('@truffle/hdwallet-provider');
+const PrivateKeys = [
+  '0x8f2a55949038a9610f50fb23b5883af3b4ecb3c3bb792cbcefbd1542c692be63',
+  '0xc87509a1c067bbde78beb793e6fa76530b6382a4c0241e5e4a9ec0a0f44dc0d3',
+  '0xae6ae8e5ccbfb04590405997ee2d52d2b330726137b875053c36d94e974d162f'
+]
+const privateKeyProvider = new PrivateKeyProvider(PrivateKeys,'http://127.0.0.1:8545', 0, 3);
 
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
@@ -27,6 +35,12 @@ module.exports = {
         return new HDWalletProvider(mnemonic, networkAddress, walletChildNum)
       },
     },
+    besu: {
+      network_id: "*",
+      provider: privateKeyProvider,
+      gas: 6700000,   // <--- Twice as much
+      gasPrice: 0x35
+    }
   },
   compilers: {
     solc: {
